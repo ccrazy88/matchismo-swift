62c1371aa8084a77d0fe992352d138e57e6dc6df
Chrisna Aing <chrisna@chrisna.org>
2014-12-08 00:57:44 -0800

    Change playing card suits to Emoji equivalents.

--- Matchismo-Swift/Model/PlayingCard.swift
+++ Matchismo-Swift/Model/PlayingCard.swift
@@ -16,7 +16,7 @@ class PlayingCard: Card {
     // Hack!
     private struct Utility {
         static let rankStrings = ["?", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        static let validSuits = ["♠︎", "♣︎", "♥︎", "♦︎"]
+        static let validSuits = ["♠️", "♣️", "♥️", "♦️"]
         static let maxRank = UInt(rankStrings.count) - 1
     }
 
